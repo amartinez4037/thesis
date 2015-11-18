7bcafd0b2517b7fa27cabaa9097ab527d0bd5109
Aron <ender4037@gmail.com>
2015-11-18 13:10:37 -0600

    testing how branch works

--- TStart.m
+++ TStart.m
@@ -15,7 +15,7 @@ clc;
 % If EEGLAB is not already in path it needs to be added or you need to cd into proper directory
 eeglab;
 
-
+ 
 %% Bring in AAR toolbox - once brought in and redrawn no need to repeat
 % eeglabRoot = fileparts(which('eeglab'));
 % url = 'https://github.com/germangh/eeglab_plugin_aar/archive/master.zip';
